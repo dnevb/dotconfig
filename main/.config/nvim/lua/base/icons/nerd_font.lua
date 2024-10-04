4fcfc290f9d0293fb3f14831ddf22d40fba71751
dvill <villalbautria08@gmail.com>
2024-10-04 12:00:11 -0500

    heirline order

--- main/.config/nvim/lua/base/icons/nerd_font.lua
+++ main/.config/nvim/lua/base/icons/nerd_font.lua
@@ -14,6 +14,7 @@ return {
   ArrowRight = "",
   Bookmarks = "",
   BufferClose = "x",
+  Codeium = "󰘦 ",
   DapBreakpoint = "",
   DapBreakpointCondition = "",
   DapBreakpointRejected = "",
--- main/.config/nvim/lua/plugins/1-base-behaviors.lua
+++ main/.config/nvim/lua/plugins/1-base-behaviors.lua
@@ -38,8 +38,8 @@ return {
     event = "User BaseDefered",
     cmd = { "Yazi", "Yazi cwd", "Yazi toggle" },
     opts = {
-        open_for_directories = true,
-        floating_window_scaling_factor = (is_android and 1.0) or 0.71
+      open_for_directories = true,
+      floating_window_scaling_factor = (is_android and 1.0) or 0.71
     },
   },
 
@@ -71,8 +71,8 @@ return {
 
       -- Don't chdir for certain buffers
       exclude_chdir = {
-        filetype = {"", "OverseerList", "alpha"},
-        buftype = {"nofile", "terminal"},
+        filetype = { "", "OverseerList", "alpha" },
+        buftype = { "nofile", "terminal" },
       },
 
       --ignore_lsp = { "lua_ls" },
@@ -127,6 +127,7 @@ return {
   -- https://github.com/max397574/better-escape.nvim
   {
     "max397574/better-escape.nvim",
+    enabled = false,
     event = "User BaseDefered",
     opts = {
       timeout = 300,
@@ -715,7 +716,7 @@ return {
       "DistroUpdateRevert"
     },
     opts = {
-        channel = "stable" -- stable/nightly
+      channel = "stable"   -- stable/nightly
     }
   },
 
